a657cbc8c39b8bac0f660c5d2e3ea5638d440a40
Nick Kvizhinadze <nickkvizhinadze@hotmail.com>
2017-09-20 13:36:32 +0400

    Add plus symbol between numbers in answer component

--- src/App.js
+++ src/App.js
@@ -62,11 +62,15 @@ const Answer = (props) => {
     return (
       <div className="col-sm-5">
         {props.selectedNumbers.map((number, i) => 
-          <span key={i}
-            onClick={() => props.unselectNumber(number)}
-        >
-            {number}
-        </span>
+        <div  key={i} className='answerContainer'>
+            <span
+                onClick={() => props.unselectNumber(number)}
+            >
+                {number}
+            </span>
+        
+            {i !== props.selectedNumbers.length - 1 ? ' + ' : ''}
+          </div>
         )}
     </div>
   );
